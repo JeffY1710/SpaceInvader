575678089a134e9765de4be36ed1674ebf03e5c4
jeff <jeff@gmail.com>
2023-03-13 20:06:31 +0100

    moving enemies fonctionnel

--- scripts/moving-enemies.js
+++ scripts/moving-enemies.js
@@ -3,7 +3,6 @@ const allCases = document.querySelectorAll('#grille div');
 let aliens = [  0,1,2,3,4,5,6,7,8,9,10,11,
                 17,18,19,20,21,22,23,24,25,26,27,28,
                 34,35,36,37,38,39,40,41,42,43,44,45];
-
 let dataRight = document.querySelectorAll("div[data='right']");
 let dataLeft = document.querySelectorAll("div[data='left']");
 
@@ -30,7 +29,6 @@ function verifyRight(){
         }
     });
     printAliens();
-
 }
 
 function verifyLeft(){  
@@ -39,13 +37,10 @@ function verifyLeft(){
             deleteAliens();
             move(17);
             direction = 1;
-
         }
     });
     printAliens();
 }
-
-
 
 function move(direction){
     let index = 0;
@@ -55,15 +50,17 @@ function move(direction){
         });
 }
 
-
 function main(){
     deleteAliens();
     move(direction);
     printAliens();
-    verifyRight();
-    verifyLeft();
+    setTimeout(() => {
+        verifyRight();
+    }, 500);
+    setTimeout(() => {
+        verifyLeft();
+    }, 500);
 }
-
 
 printAliens();
     setInterval(()=>{
