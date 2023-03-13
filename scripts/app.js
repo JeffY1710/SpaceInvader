6375cf691b4d5bff2f6c03f322ddd96db6fa5a5e
Val <pro.mvaldo@gmail.com>
2023-03-13 10:32:26 +0100

    base project

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+const gameGrid = document.querySelector('#grille');
+
+const generateGrid = function (){
+    for (let index = 0; index < 255; index++) {
+       const gameCase = document.createElement('div');
+       gameGrid.appendChild(gameCase);
+        
+    }
+}
+
+generateGrid();
